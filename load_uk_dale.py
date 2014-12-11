d46ae2ccd84e91fd47f3eb10eff92b7d7dd23bb5
Jack Kelly <jack-list@xlk.org.uk>
2014-12-11 16:57:54 +0000

    add timeframe window

--- load_uk_dale.py
+++ load_uk_dale.py
@@ -1,5 +1,11 @@
-from nilmtk import DataSet
+from nilmtk import DataSet, TimeFrame
+import pandas as pd
 
 ukdale = DataSet('/data/mine/vadeec/merged/ukdale.h5')
+TZ = 'Europe/London'
+ukdale.store.window = TimeFrame(pd.Timestamp("2014-01-01 00:00", tz=TZ),
+                                pd.Timestamp("2014-01-02 00:00", tz=TZ))
+
+
 elec = ukdale.buildings[1].elec
 elec.use_alternative_mains()
