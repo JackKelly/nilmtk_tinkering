8ea88e8a3d5cb6f43e55a9651114324e7f2a9917
Jack Kelly <jack-list@xlk.org.uk>
2014-12-12 10:11:39 +0000

    using new set_window

--- load_uk_dale.py
+++ load_uk_dale.py
@@ -2,10 +2,11 @@ from nilmtk import DataSet, TimeFrame
 import pandas as pd
 
 ukdale = DataSet('/data/mine/vadeec/merged/ukdale.h5')
-TZ = 'Europe/London'
-ukdale.store.window = TimeFrame(pd.Timestamp("2014-01-01 00:00", tz=TZ),
-                                pd.Timestamp("2014-01-02 00:00", tz=TZ))
+# TZ = 'Europe/London'
+# ukdale.store.window = TimeFrame(pd.Timestamp("2014-01-01 00:00", tz=TZ),
+#                                 pd.Timestamp("2014-01-02 00:00", tz=TZ))
 
+ukdale.set_window("2014-01-01", "2014-01-02")
 
 elec = ukdale.buildings[1].elec
 elec.use_alternative_mains()
